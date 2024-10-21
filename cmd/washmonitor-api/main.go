25432f1e50a68eabb1609b799d885796f31e1e8b
Mason <masoncfrancis@gmail.com>
2024-10-20 22:57:35 -0400

    Added mongo as dependency, added code to parse incoming request

--- cmd/washmonitor-api/main.go
+++ cmd/washmonitor-api/main.go
@@ -1,6 +1,36 @@
 package washmonitor_api
 
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+)
+
 // Main function
 func main() {
 	println("Hello, World!")
+
+	app := fiber.New()
+
+	app.Post("/sendMessage", func(c *fiber.Ctx) error {
+
+		// Define the request body
+		type RequestMessageBody struct {
+			Message  string `json:"message"`
+			DeviceId string `json:"deviceId"`
+		}
+
+		// Parse the request body
+		apiKey := c.Get("X-API-Key")
+
+		var requestBody RequestMessageBody
+		if err := c.BodyParser(&requestBody); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		// Connect to mongodb and check
+
+	})
+
 }
