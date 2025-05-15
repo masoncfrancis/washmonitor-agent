20cba5a0a26fcf0a930217b15678e33201c4baac
Mason <masoncfrancis@gmail.com>
2025-05-15 13:48:49 -0400

    Changed button to take up whole screen while running

--- dashboard/src/components/LaundryDashboard.tsx
+++ dashboard/src/components/LaundryDashboard.tsx
@@ -7,41 +7,56 @@ const LaundryDashboard = () => {
 
     const handleButtonClick = (type: 'washer' | 'dryer') => {
         setLoading(true);
-        const apiUrl = '/';
-
-        fetch(apiUrl, {method: 'GET'})
-            .then(response => {
-                if (response.ok) {
-                    if (type === 'washer') {
-                        setWasherActive(!washerActive);
-                    } else {
-                        setDryerActive(!dryerActive);
-                    }
-                }
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                setLoading(false);
-            });
+        setTimeout(() => {
+            if (type === 'washer') {
+                setWasherActive(!washerActive);
+                if (dryerActive) setDryerActive(false);
+            } else {
+                setDryerActive(!dryerActive);
+                if (washerActive) setWasherActive(false);
+            }
+            setLoading(false);
+        }, 300);
     };
 
     return (
         <div className="flex h-screen w-screen">
-            <div
-                className={`flex-1 flex flex-col justify-center items-center text-4xl cursor-pointer text-center break-words ${washerActive ? 'bg-green-500 text-white' : 'bg-green-500 text-white'}`}
-                onClick={() => handleButtonClick('washer')}
-            >
-                {washerActive ? 'Monitoring Washer' : 'Washer'}
-                {washerActive && <div className="loader mt-4"></div>}
-            </div>
-            <div
-                className={`flex-1 flex flex-col justify-center items-center text-4xl cursor-pointer text-center break-words ${dryerActive ? 'bg-blue-500 text-white' : 'bg-blue-500 text-white'}`}
-                onClick={() => handleButtonClick('dryer')}
-            >
-                {dryerActive ? 'Monitoring Dryer' : 'Dryer'}
-                {dryerActive && <div className="loader mt-4"></div>}
-            </div>
+            {(!dryerActive && !washerActive) && (
+                <>
+                    <div
+                        className={`flex-1 flex flex-col justify-center items-center text-4xl cursor-pointer text-center break-words bg-green-500 text-white`}
+                        onClick={() => handleButtonClick('washer')}
+                    >
+                        {washerActive ? 'Monitoring Washer' : 'Washer'}
+                        {washerActive && <div className="loader mt-4"></div>}
+                    </div>
+                    <div
+                        className={`flex-1 flex flex-col justify-center items-center text-4xl cursor-pointer text-center break-words bg-blue-500 text-white`}
+                        onClick={() => handleButtonClick('dryer')}
+                    >
+                        {dryerActive ? 'Monitoring Dryer' : 'Dryer'}
+                        {dryerActive && <div className="loader mt-4"></div>}
+                    </div>
+                </>
+            )}
+            {washerActive && !dryerActive && (
+                <div
+                    className="flex-1 flex flex-col justify-center items-center text-4xl cursor-pointer text-center break-words bg-green-500 text-white h-full w-full"
+                    onClick={() => handleButtonClick('washer')}
+                >
+                    Monitoring Washer
+                    <div className="loader mt-4"></div>
+                </div>
+            )}
+            {dryerActive && !washerActive && (
+                <div
+                    className="flex-1 flex flex-col justify-center items-center text-4xl cursor-pointer text-center break-words bg-blue-500 text-white h-full w-full"
+                    onClick={() => handleButtonClick('dryer')}
+                >
+                    Monitoring Dryer
+                    <div className="loader mt-4"></div>
+                </div>
+            )}
             {loading && (
                 <div
                     className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50 transition-opacity duration-500">
